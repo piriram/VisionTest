c5922a33abd37a8620b6f337e6f168676b6e9128
piri <pyoram25@gmail.com>
2024-09-20 20:40:53 +0900

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by ram on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
